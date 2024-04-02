f583eb5ade16ef1cf978e84605c6bcee613c6155
StonecoldLi <lijingfan2002@126.com>
2024-04-02 20:39:34 +0800

    auto v4.1 finished ANSYS APDL PART

--- auto_version/TD_Reconstruction/add_id.py
+++ auto_version/TD_Reconstruction/add_id.py
@@ -2,10 +2,10 @@ import pandas as pd
 # 以前面创建的CSV文件为例，添加'id'列
 #df = pd.read_csv('../data_merged/plane3merge_cp.csv')
 
-def add_id(csv_file,updated_csv_file_path):
+def add_id(csv_file,flag,updated_csv_file_path):
     df = pd.read_csv(csv_file)
 # 添加'id'列，从1开始
-    df['id'] = ['3' + str(i+1) for i in range(len(df))]
+    df['id'] = [str(flag+1) + str(i+1) for i in range(len(df))] #命名规则！
 
 # 由于原始CSV文件没有'id'列，这里将'id'列移动到最前面
     df = df[['id'] + [col for col in df.columns if col != 'id']]
