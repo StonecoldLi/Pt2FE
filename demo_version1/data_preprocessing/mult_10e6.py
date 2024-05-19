f9def4a6b7b8d2fc69b86a0df9099e5ddf19b480
unknown <lijingfan2002@126.com>
2024-05-19 23:32:16 +0800

    released version 1.0

--- demo_version1/data_preprocessing/mult_10e6.py
+++
@@ -1,9 +0,0 @@
-# 打开原始txt文件和目标txt文件
-with open('plane_data_org.txt', 'r') as f_in, open('plane_data_e6.txt', 'w') as f_out:
-    # 逐行读取原始文件
-    for line in f_in:
-        # 将每行数据按空格分割
-        data = line.split()
-        # 将每个值乘以100并转换为字符串，然后用空格连接并写入目标文件
-        multiplied_data = [str(float(val) * 1000000) for val in data]
-        f_out.write(' '.join(multiplied_data) + '\n')
--- demo_version1/format transformation/pcd2txt.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# 读取PCD文件
-pcd_data = pd.read_csv('ascii_change.pcd', sep=' ', skiprows=11, header=None, names=['x', 'y', 'z'])
-
-# 保存为TXT文件
-pcd_data.to_csv('plane_data_org.txt', sep=' ', index=False)
